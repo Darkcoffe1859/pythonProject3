0d19dd8c252d83bcb5634a56b230829082375a15
Darkcoffe1859 <shency49@gmail.com>
2023-05-20 14:16:53 +0100

    calculation in python

---
+++ my first project in python.py
@@ -0,0 +1,18 @@
+# A program that print hello world
+print("Hello World")
+
+# Calculator
+# write a program that takes two numbers as input from the user.
+#perform the following operations on the numbers: adition, subtraction, multiplication, and division
+#print the result of each operation
+
+first_number = float (input("enter the first number:"))
+second_number = float(input("enter the second number:"))
+Addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+print(Addition)
+print(subtraction)
+print(multiplication)
+print(division)
